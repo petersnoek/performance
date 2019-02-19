4b45e9d048b5de8f0a43fa3261636c9d79f84210
Peter Snoek <petersnoek@davinci.nl>
2019-02-19 17:04:47 +0100

    laravel 5.7 install

---
+++ database/migrations/2019_02_19_154714_create_address_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAddressTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('address', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('street');
+            $table->unsignedInteger('number');
+            $table->string('suffix');
+            $table->string('postalcode');
+            $table->string('city');
+            $table->string('country');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('address');
+    }
+}
---
+++ database/seeds/addressSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Carbon\Carbon;
+
+class addressSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker\Factory::create();
+
+        $this->command->info("Generating 10.000 addresses (batch " . $y . ")");
+
+        for ($y = 0; $y <= 100; $y++) {
+            for ($x = 0; $x <= 10000; $x++) {
+                DB::table('address')->insert([
+                    'street' => $faker->word . 'straat',
+                    'number' => $faker->numberBetween(1, 100),
+                    'suffix' => $faker->randomLetter(),
+                    'postalcode' => $faker->numberBetween(1000,
+                            9999) . strtoupper($faker->randomLetter() . $faker->randomLetter()),
+                    'city' => $faker->city,
+                    'country' => $faker->country,
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now(),
+                ]);
+            }
+
+        }
+    }
+}
+
+
